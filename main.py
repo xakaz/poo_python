22fdb4269ef6a9565dc00dc16a5ad2af6f658c41
Loïc <loic.hernandez@sfr.fr>
2024-11-20 20:20:14 +0100

    add est_majeur attribute

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 class Personne:
-    def __init__(self, nom:str, prenom:str, age:int)->None:
+    def __init__(self)->None:
         self.nom = input("Entrez votre nom: ")
         self.prenom = input("Entrez votre prénom: ")
         self.age = input("Entrez votre âge: ")
+        self.est_majeur = True if int(self.age) >= 18 else False
 
     def se_presenter(self)->str:
-        return f"Bonjour, je m'appelle {self.prenom} {self.nom} et j'ai {self.age} ans."
+        return f"Bonjour, je m'appelle {self.prenom} {self.nom} et j'ai {self.age} ans. Je suis {'majeur' if self.est_majeur else 'mineur'}."
 
 
-personne1 = Personne("Doe", "John", 30)
+personne1 = Personne()
 print(personne1.se_presenter())
